4c8e15618086525bed237db9dd7e66517ed0b795
schmida736 <schmidtd.mail@gmail.com>
2021-04-26 15:12:05 +0200

    CodeExecution prototype

--- Pic-Simulator/Pic-Simulator/CodeExecution.cs
+++ Pic-Simulator/Pic-Simulator/CodeExecution.cs
@@ -6,6 +6,27 @@ namespace Pic_Simulator
 {
     static class CodeExecution
     {
-       //public bool executeInstruction();
+
+        public enum Instruction
+        {
+            
+        }
+
+        public (byte, byte, byte, byte) Fetch()
+        {
+
+        }
+
+        public bool Execute(Instruction instruction, byte param1, byte param2)
+        {
+            switch (instruction)
+            {
+                case Instruction.MOVLW:
+                    break;
+                default:
+                    return false;
+                    
+            }
+        }
     }
 }
