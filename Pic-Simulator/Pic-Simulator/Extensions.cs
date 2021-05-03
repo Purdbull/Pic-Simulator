7efad2f565060d787a9f263dcaf773302bed2cb6
Purdbull <72924017+Purdbull@users.noreply.github.com>
2021-05-03 14:30:05 +0200

    Added ExtensionMethods and GetBit method

---
+++ Pic-Simulator/Pic-Simulator/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    public static class Extensions
+    {
+        private static ushort bitMask = 0b_00000000_00000001;
+        public static bool GetBit(this UInt16 data, int index)
+        {
+            for (int i = 15; i > index; i--)
+            {
+                bitMask = (ushort)(bitMask << 1);
+            }
+            data = (ushort)(data & bitMask);
+
+            return data > 0;
+        }
+    }
+}
