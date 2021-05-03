092e3f5f2730dbbb65cef7cb99abe1234c5e80fe
Purdbull <72924017+Purdbull@users.noreply.github.com>
2021-05-03 20:03:00 +0200

    added GetBankByte

--- Pic-Simulator/Pic-Simulator/DataMemory.cs
+++ Pic-Simulator/Pic-Simulator/DataMemory.cs
@@ -7,5 +7,24 @@ namespace Pic_Simulator
     public class DataMemory : Memory<UInt16>
     {
         public DataMemory() : base(PIC.MAX_DATAMEM_SIZE) {}
+        public byte GetBankByte(int bank, byte adress)
+        {
+            ushort bothBankBytes = this.GetValue(adress);
+
+            if (bank == 1) 
+            {
+                bothBankBytes = (ushort)(bothBankBytes >> 8);
+                return (byte)(bothBankBytes);
+            }
+            else if (bank == 2)
+            {
+                return (byte)(bothBankBytes);
+            }
+            else
+            {
+                //TODO: Exception?
+                return 0;
+            }
+        }
     }
 }
