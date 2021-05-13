2e5ef3243519579d341f550d8e0049d9915d5e01
Purdbull <72924017+Purdbull@users.noreply.github.com>
2021-05-13 18:03:26 +0200

    Added Swopp

--- Pic-Simulator/Pic-Simulator/CodeExecution.cs
+++ Pic-Simulator/Pic-Simulator/CodeExecution.cs
@@ -9,73 +9,77 @@ namespace Pic_Simulator
     {
         public enum Instruction
         {
-            NOP    = 0b_00000000_00000000,  //0000 0000 0xx0 0000
-            MOVLW  = 0b_00110000_00000000,  //0011 00xx kkkk kkkk
-            RETLW  = 0b_00110100_00000000,  //0011 01xx kkkk kkkk
-            ADDLW  = 0b_00111110_00000000,  //0011 111x kkkk kkkk
-            SUBLW  = 0b_00111100_00000000,  //0011 110x kkkk kkkk
-            CALL   = 0b_00100000_00000000,  //0010 0kkk kkkk kkkk
-            GOTO   = 0b_00101000_00000000,  //0010 1kkk kkkk kkkk
-            MOVWF  = 0b_00000000_10000000,  //0000 0000 1fff ffff
-            MOVF   = 0b_00001000_00000000,  //0000 1000 dfff ffff
-            CLRF   = 0b_00000001_10000000,  //0000 0001 1fff ffff
-            CLRW   = 0b_00000001_00000000,  //0000 0001 0xxx xxxx
-            SUBWF  = 0b_00000010_00000000,  //0000 0010 dfff ffff
-            ADDWF  = 0b_00000111_00000000,  //0000 0111 dfff ffff
-            DECFSZ = 0b_00001011_00000000,  //0000 1011 dfff ffff
-            INCFSZ = 0b_00001111_00000000,  //0000 1111 dfff ffff
-            RLF    = 0b_00001101_00000000,  //0000 1100 dfff ffff
-            RRF    = 0b_00001100_00000000,  //0000 1100 dfff ffff
-            BSF    = 0b_00010100_00000000,  //0001 01bb bfff ffff
-            BCF    = 0b_00010000_00000000,  //0001 00bb bfff ffff
-            BTFSC  = 0b_00011000_00000000,  //0001 10bb bfff ffff
-            BTFSS  = 0b_00011100_00000000,  //0001 11bb bfff ffff
-            XORWF  = 0b_00000110_00000000,  //0000 0110 dfff ffff
-            XORLW  = 0b_00111010_00000000,  //0011 1010 kkkk kkkk
-            ANDLW  = 0b_00111001_00000000,  //0011 1001 kkkk kkkk
-            ANDWF  = 0b_00000101_00000000,  //0000 0101 dfff ffff
-            IORLW  = 0b_00111000_00000000,  //0011 1000 kkkk kkkk
-            IORWF  = 0b_00000100_00000000,  //0000 0100 dfff ffff
-            INCF   = 0b_00001010_00000000,  //0000 1010 dfff ffff
-            DECF   = 0b_00000011_00000000,  //0000 0011 dfff ffff
-            RETURN = 0b_00000000_00001000   //0000 0000 0000 1000
+            NOP     = 0b_00000000_00000000,     //0000 0000 0xx0 0000
+            MOVLW   = 0b_00110000_00000000,     //0011 00xx kkkk kkkk
+            RETLW   = 0b_00110100_00000000,     //0011 01xx kkkk kkkk
+            ADDLW   = 0b_00111110_00000000,     //0011 111x kkkk kkkk
+            SUBLW   = 0b_00111100_00000000,     //0011 110x kkkk kkkk
+            CALL    = 0b_00100000_00000000,     //0010 0kkk kkkk kkkk
+            GOTO    = 0b_00101000_00000000,     //0010 1kkk kkkk kkkk
+            MOVWF   = 0b_00000000_10000000,     //0000 0000 1fff ffff
+            MOVF    = 0b_00001000_00000000,     //0000 1000 dfff ffff
+            CLRF    = 0b_00000001_10000000,     //0000 0001 1fff ffff
+            CLRW    = 0b_00000001_00000000,     //0000 0001 0xxx xxxx
+            SUBWF   = 0b_00000010_00000000,     //0000 0010 dfff ffff
+            ADDWF   = 0b_00000111_00000000,     //0000 0111 dfff ffff
+            DECFSZ  = 0b_00001011_00000000,     //0000 1011 dfff ffff
+            INCFSZ  = 0b_00001111_00000000,     //0000 1111 dfff ffff
+            RLF     = 0b_00001101_00000000,     //0000 1100 dfff ffff
+            RRF     = 0b_00001100_00000000,     //0000 1100 dfff ffff
+            BSF     = 0b_00010100_00000000,     //0001 01bb bfff ffff
+            BCF     = 0b_00010000_00000000,     //0001 00bb bfff ffff
+            BTFSC   = 0b_00011000_00000000,     //0001 10bb bfff ffff
+            BTFSS   = 0b_00011100_00000000,     //0001 11bb bfff ffff
+            XORWF   = 0b_00000110_00000000,     //0000 0110 dfff ffff
+            XORLW   = 0b_00111010_00000000,     //0011 1010 kkkk kkkk
+            ANDLW   = 0b_00111001_00000000,     //0011 1001 kkkk kkkk
+            ANDWF   = 0b_00000101_00000000,     //0000 0101 dfff ffff
+            IORLW   = 0b_00111000_00000000,     //0011 1000 kkkk kkkk
+            IORWF   = 0b_00000100_00000000,     //0000 0100 dfff ffff
+            INCF    = 0b_00001010_00000000,     //0000 1010 dfff ffff
+            DECF    = 0b_00000011_00000000,     //0000 0011 dfff ffff
+            RETURN  = 0b_00000000_00001000,     //0000 0000 0000 1000
+            COMF    = 0b_00001001_00000000,     //0000 1001 dfff ffff
+            SWAP    = 0b_00001110_00000000      //0000 1110 dfff ffff
         }
 
         public enum InstructionMask
         {
-            XORWF  = 0b_11111111_00000000,  //0000 0110 dfff ffff
-            XORLW  = 0b_11111111_00000000,  //0011 1010 kkkk kkkk
-            ANDLW  = 0b_11111111_00000000,  //0011 1001 kkkk kkkk
-            ANDWF  = 0b_11111111_00000000,  //0000 0101 dfff ffff
-            IORLW  = 0b_11111111_00000000,  //0011 1000 kkkk kkkk
-            IORWF  = 0b_11111111_00000000,  //0000 0100 dfff ffff
-            SUBWF  = 0b_11111111_00000000,  //0000 0010 dfff ffff
-            ADDWF  = 0b_11111111_00000000,  //0000 0111 dfff ffff
-            DECFSZ = 0b_11111111_00000000,  //0000 1011 dfff ffff
-            INCFSZ = 0b_11111111_00000000,  //0000 1111 dfff ffff
-            RLF    = 0b_11111111_00000000,  //0000 1100 dfff ffff
-            RRF    = 0b_11111111_00000000,  //0000 1100 dfff ffff
-            MOVF   = 0b_11111111_00000000,  //0000 1000 dfff ffff
-            INCF   = 0b_11111111_00000000,  //0000 1010 dfff ffff
-            DECF   = 0b_11111111_00000000,  //0000 0011 dfff ffff
+            XORWF   = 0b_11111111_00000000,     //0000 0110 dfff ffff
+            XORLW   = 0b_11111111_00000000,     //0011 1010 kkkk kkkk
+            ANDLW   = 0b_11111111_00000000,     //0011 1001 kkkk kkkk
+            ANDWF   = 0b_11111111_00000000,     //0000 0101 dfff ffff
+            IORLW   = 0b_11111111_00000000,     //0011 1000 kkkk kkkk
+            IORWF   = 0b_11111111_00000000,     //0000 0100 dfff ffff
+            SUBWF   = 0b_11111111_00000000,     //0000 0010 dfff ffff
+            ADDWF   = 0b_11111111_00000000,     //0000 0111 dfff ffff
+            DECFSZ  = 0b_11111111_00000000,     //0000 1011 dfff ffff
+            INCFSZ  = 0b_11111111_00000000,     //0000 1111 dfff ffff
+            RLF     = 0b_11111111_00000000,     //0000 1100 dfff ffff
+            RRF     = 0b_11111111_00000000,     //0000 1100 dfff ffff
+            MOVF    = 0b_11111111_00000000,     //0000 1000 dfff ffff
+            INCF    = 0b_11111111_00000000,     //0000 1010 dfff ffff
+            DECF    = 0b_11111111_00000000,     //0000 0011 dfff ffff
+            COMF    = 0b_11111111_00000000,     //0000 1001 dfff ffff
+            SWAP    = 0b_11111111_00000000,     //0000 1110 dfff ffff
 
-            MOVWF  = 0b_11111111_10000000,  //0000 0000 1fff ffff
-            CLRF   = 0b_11111111_10000000,  //0000 0001 1fff ffff
-            CLRW   = 0b_11111111_10000000,  //0000 0001 0xxx xxxx
+            MOVWF   = 0b_11111111_10000000,     //0000 0000 1fff ffff
+            CLRF    = 0b_11111111_10000000,     //0000 0001 1fff ffff
+            CLRW    = 0b_11111111_10000000,     //0000 0001 0xxx xxxx
 
-            NOP    = 0b_11111111_10011111,  //0000 0000 0xx0 0000
+            NOP     = 0b_11111111_10011111,     //0000 0000 0xx0 0000
 
-            ADDLW  = 0b_11111110_00000000,  //0011 111x kkkk kkkk
-            MOVLW  = 0b_11111100_00000000,  //0011 00xx kkkk kkkk
-            RETLW  = 0b_11111100_00000000,  //0011 01xx kkkk kkkk
-            SUBLW  = 0b_11111110_00000000,  //0011 110x kkkk kkkk
-            BSF    = 0b_11111100_00000000,  //0001 01bb bfff ffff
-            BCF    = 0b_11111100_00000000,  //0001 00bb bfff ffff
-            BTFSC  = 0b_11111100_00000000,  //0001 10bb bfff ffff
-            BTFSS  = 0b_11111100_00000000,  //0001 11bb bfff ffff
-            CALL   = 0b_11111000_00000000,  //0010 0kkk kkkk kkkk
-            GOTO   = 0b_11111000_00000000,  //0010 1kkk kkkk kkkk
-            RETURN = 0b_11111111_11111111   //0000 0000 0000 1000
+            ADDLW   = 0b_11111110_00000000,     //0011 111x kkkk kkkk
+            MOVLW   = 0b_11111100_00000000,     //0011 00xx kkkk kkkk
+            RETLW   = 0b_11111100_00000000,     //0011 01xx kkkk kkkk
+            SUBLW   = 0b_11111110_00000000,     //0011 110x kkkk kkkk
+            BSF     = 0b_11111100_00000000,     //0001 01bb bfff ffff
+            BCF     = 0b_11111100_00000000,     //0001 00bb bfff ffff
+            BTFSC   = 0b_11111100_00000000,     //0001 10bb bfff ffff
+            BTFSS   = 0b_11111100_00000000,     //0001 11bb bfff ffff
+            CALL    = 0b_11111000_00000000,     //0010 0kkk kkkk kkkk
+            GOTO    = 0b_11111000_00000000,     //0010 1kkk kkkk kkkk
+            RETURN  = 0b_11111111_11111111      //0000 0000 0000 1000
         }
 
         public static UInt16 Fetch()
@@ -336,6 +340,29 @@ namespace Pic_Simulator
                 case Instruction.CLRW:
                     Program.pic.wReg.SetValue(0);
                     Program.pic.dataMem.SetFlag(statusAdress, 2); //setting z-flag
+                    return true;
+
+                case Instruction.COMF:
+                    param = (byte)(data);
+                    dataMemAddress = (byte)(param & 0b_01111111);
+                    result = (byte)(~Program.pic.dataMem.GetValue(dataMemAddress));
+                    if (data.GetBit(destinationBitIndex))
+                    {
+                        Program.pic.dataMem.Set(dataMemAddress, result);
+                    }
+                    else
+                    {
+                        Program.pic.wReg.SetValue(result);
+                    }
+
+                    if (result > 0)
+                    {
+                        Program.pic.dataMem.ClearFlag(statusAdress, 2); //clearing z-flag
+                    }
+                    else
+                    {
+                        Program.pic.dataMem.SetFlag(statusAdress, 2); //setting z-flag
+                    }
                     return true;
 
                 case Instruction.DECFSZ:
@@ -631,12 +658,12 @@ namespace Pic_Simulator
                     if (overflowCheck >= 0)
                     {
                         Program.pic.dataMem.SetFlag(statusAdress, 0); //setting c-flag
-                        Program.pic.dataMem.ClearFlag(statusAdress, 1); //clearing dc-flag
+                        Program.pic.dataMem.SetFlag(statusAdress, 1); //setting dc-flag
                     }
                     else
                     {
                         Program.pic.dataMem.ClearFlag(statusAdress, 0); //clearing c-flag
-                        Program.pic.dataMem.SetFlag(statusAdress, 1); //setting dc-flag
+                        Program.pic.dataMem.ClearFlag(statusAdress, 1); //clearing dc-flag
                     }
 
                     if (operand2 == 0)
@@ -646,6 +673,23 @@ namespace Pic_Simulator
                     }
 
                     return true;
+
+                case Instruction.SWAP:
+                    param = (byte)(data);
+                    dataMemAddress = (byte)(param & 0b_01111111);
+                    operand1 = (byte)(Program.pic.dataMem.GetValue(dataMemAddress));
+                    operand2 = (byte)(operand1 << 4);
+                    operand1 = (byte)(operand1 >> 4);
+                    result = (byte)(operand1 + operand2);
+                    if (data.GetBit(destinationBitIndex))
+                    {
+                        Program.pic.dataMem.Set(dataMemAddress, result);
+                    }
+                    else
+                    {
+                        Program.pic.wReg.SetValue(result);
+                    }
+                    return true; 
 
                 case Instruction.XORLW:
                     result = (byte)(Program.pic.wReg.GetValue() ^ (byte)(data));
